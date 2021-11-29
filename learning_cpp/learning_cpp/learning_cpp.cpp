543701e1a9d62c92b7a5899251bf9cbdf229ac73
Hakkı Koçar <18701025@mersin.edu.tr>
2021-11-30 00:59:20 +0300

    last

---
+++ learning_cpp/learning_cpp/learning_cpp.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	/*int sayi = 6;
+	int sayi2 = 7;
+	int sonuc ;
+
+	sonuc = sayi + sayi2;
+
+	cout << sonuc ;*/
+
+	// boolean
+	 //   bool boolean1 = 1; 1 //1 yazmakla true yazmak ve 0 yazmakla false yazmak arasında fark yok
+		//bool boolean2 = 0; 0
+		//bool boolean3 = true; 1
+		//bool boolean4 = false; 0
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	////pointer (referanslama)
+	//int sayi1 = 10;
+	//int *sayi1Ptr; //pointerı bir değişken tanımlarken değişkenin türünün yanına ya da değişkenin yanına * işareti koyulur. eğer koyulmazsa hata verir.
+	//sayi1Ptr = &sayi1; //pointerın sayi1 verilerini alabilesi için tanımlama yapma
+
+	////*sayi1Ptr = 500;//pointer ile aldığım verinin değerini değiştirmek için başına yıldız koyulur ve böylede sayi1 yeni değeri 500 olur
+
+	//cout <<"sayi 1 ramdeki konumu"<<"  "<< &sayi1<<endl;//sayı 1in adresini almak için önüne & işareti konulur bu onun ramdeki yerini göstermeye yarar
+	//cout <<"sayi 1 degeri" <<"  "<< sayi1<<endl; //sayi 1in değerini yazdırır.
+	//cout << "sayiPtr sayi1den konumunu alir" << "  " << sayi1Ptr << endl; //sayi1in referansını sayi1Ptrye göndermesi
+	//cout << "sayi1Ptr ramdeki konumu" << "  " << &sayi1Ptr << endl;//sayi1Ptr ramdeki yeri
+	//cout << "sayiPtr ile sayi1 degerine ulasma" << "  " << *sayi1Ptr << endl; //sayi1 değerini pointer ile sayi1Ptr ile alma 
+
+	// * işareti değere gitme
+	// & bellek adresine gitme
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//DEnklikler
+	// ==  Denklik anlamına gelir (ise)
+	// !=  denk değildir (değiş ise)
+	//  > büyüktür
+	//  < küçüktür
+	//  >= ya da >= büyük eşit ya da büyük eşit
+	//  && ve bağlama aracı
+	//  || veya bağlama aracı
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	//İF KULLANIMI
+
+	//int ifi = 4;
+
+	//
+	//if (ifi == 4)
+	//{
+	//	cout << "sayi 5'den kucuk" << endl;
+	//}
+
+	//else
+	//{
+	//	cout << "sayi 5'den buyuk" << endl;
+	//}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+}
